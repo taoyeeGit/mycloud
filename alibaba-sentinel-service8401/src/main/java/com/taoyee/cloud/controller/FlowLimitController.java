6b7196d953afdabb90fc46c791fc43630c9034ba
taoyeeGit <taoyee@139.com>
2022-09-25 08:46:19 +0800

    增加对 sentinel 的持久化

--- alibaba-sentinel-service8401/src/main/java/com/taoyee/cloud/controller/FlowLimitController.java
+++ alibaba-sentinel-service8401/src/main/java/com/taoyee/cloud/controller/FlowLimitController.java
@@ -1,5 +1,9 @@
 package com.taoyee.cloud.controller;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
+import com.taoyee.cloud.entities.CommonResult;
+import com.taoyee.cloud.entities.Payment;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,10 +18,22 @@ public class FlowLimitController {
 
     @GetMapping(value = "/testA")
     public String testA(){
+        log.info( Thread.currentThread().getName()+"--------->testA");
         return  "TestA------->端口： "+serverPort;
     }
     @GetMapping(value = "/testB")
     public String testB(){
         return  "TestB------->端口： "+serverPort;
     }
+
+    @GetMapping("/byResource")
+    @SentinelResource(value = "byResource",blockHandler ="handlerException",fallback ="fallbackException" )
+    public CommonResult byResource(){
+        log.info("1111111111111");
+        return new CommonResult(200,"成功!",new Payment(2020l,"serial001"));
+    }
+    public CommonResult handlerException(BlockException blockException){
+        return new CommonResult(200,blockException.getClass().getCanonicalName()+"\t  服务不可用!");
+    }
+
 }
